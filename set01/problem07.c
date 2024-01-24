9db4bd1cb725049af1da54af9c31d98c860cf619
Riya <riyahs.bsc23@rvu.edu.in>
2024-01-24 21:13:50 +0530

    test

---
+++ set01/problem07.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int input_n()
+{
+    int n;
+    printf("Enter a value of number:");
+    scanf("%d",&n);
+    return n;
+}
+
+int sum_n_nos(int n)
+{
+    int sum=0;
+    for (int i=1;i<=n; i++)
+    {
+        sum +=i;
+    }
+    return sum;
+}
+void output(int n,int sum)
+{
+    printf("The sum of natural numbers up to %d is :%d\n",n,sum);
+}
+int main()
+{
+    int num = input_n();
+    int sum = sum_n_nos(num);
+    output(num,sum);
+    return 0;
+}
